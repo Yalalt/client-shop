a0c68cbafd2342a95bb12d7aa84663272d1b59eb
Yalalt <yalaltgandush@gmail.com>
2023-01-16 09:43:29 +0800

    images upoads:

--- src/component/subComponent/NavBarTop.jsx
+++ src/component/subComponent/NavBarTop.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { HeaderLogoBig } from "../../utils/imgs/headerLogoElectronic.png";
 import style from "../../styles/navbar.module.css";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className={style.nav}>
-      <Link to="/" className={style.logoNavbar}><img src="/" alt="Logo here" /></Link>
+      <Link to="/" className={style.logoNavbar}>
+        <img src={HeaderLogoBig} alt="Logo here" />
+      </Link>
       <ul className={style.menuItems}>
-          <CustomLink key={index} to={menu.url}>
-            Login
-          </CustomLink>
+        <CustomLink key={index} to={menu.url}>
+          Login
+        </CustomLink>
       </ul>
     </nav>
   );
@@ -20,8 +23,10 @@ function CustomLink({ to, children, ...props }) {
   const isActive = useMatch({ path: resolvedPath.pathname, end: true });
   return (
     <li>
-        {isActive}
-      <Link to={to} {...props}>{children}</Link>
+      {isActive}
+      <Link to={to} {...props}>
+        {children}
+      </Link>
     </li>
   );
 }
